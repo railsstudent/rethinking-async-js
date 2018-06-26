8b1a07c697c7b57250afa62d51f7f3fdc1b929fa
Connie Leung <connie.leung@nixplay.com>
2018-06-26 13:45:03 +0800

    solution of exercise 3

--- exercises/ex3/ex3.js
+++ exercises/ex3/ex3.js
@@ -20,8 +20,25 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	return new Promise(function executor(resolve) {
+		fakeAjax(file, resolve);
+	});
 }
 
-// request all files at once in "parallel"
-// ???
+var p1 = getFile('file1');
+var p2 = getFile('file2');
+var p3 = getFile('file3');
+
+p1
+.then(output)
+.then(function() {
+	return p2;
+})
+.then(output)
+.then(function() {
+	return p3;
+})
+.then(output)
+.then(function() {
+	output('Complete!');
+});
