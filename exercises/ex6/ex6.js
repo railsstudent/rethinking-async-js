b723f92e9116dcbeed1d1cd6074786920583da5f
Connie Leung <cleung334@gmail.com>
2018-06-30 13:54:55 +0800

    Exercise 6 solution

--- exercises/ex6/ex6.js
+++ exercises/ex6/ex6.js
@@ -1,35 +1,38 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "The first text",
+    file2: "The middle text",
+    file3: "The last text"
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function() {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-	return ASQ(function(done){
-		fakeAjax(file,done);
-	});
+  return ASQ(function(done) {
+    fakeAjax(file, done);
+  });
 }
 
-// Request all files at once in
-// "parallel" via `getFile(..)`.
-//
-// Render as each one finishes,
-// but only once previous rendering
-// is done.
-
-// ???
+ASQ()
+  .seq(
+    ...["file1", "file2", "file3"].map(getFile).map(function(sq) {
+      return function() {
+        return sq.val(output);
+      };
+    })
+  )
+  .val(function() {
+    output("Complete!!!");
+  });
